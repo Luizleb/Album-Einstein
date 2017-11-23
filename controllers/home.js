5da82e1f479ae8d3db56276717d581794469effb
Luizleb <luizleb@gmail.com>
2017-11-23 15:16:52 -0200

    With Mongo

--- controllers/home.js
+++ controllers/home.js
@@ -1,39 +1,19 @@
 var sidebar = require('../helpers/sidebar');
+var imageModel = require('../models/image');
 
 var model = {
-    images: [
-        {
-            uniqueId:       1,
-            title:          'Sample Image 1',
-            description:    'Image 1 description',
-            filename:       'image1.jpg',
-            views:          0,
-            likes:          0,
-            timestamp:      Date.now()  
-        }, {
-            uniqueId:       2,
-            title:          'Sample Image 2',
-            description:    'Image 2 description',
-            filename:       'image2.jpg',
-            views:          0,
-            likes:          0,
-            timestamp:      Date.now()
-        }, {
-            uniqueId:       3,
-            title:          'Sample Image 3',
-            description:    'Image 3 description',
-            filename:       'image3.jpg',
-            views:          0,
-            likes:          0,
-            timestamp:      Date.now()
-        }
-    ]
+    images: []
 };
 
 module.exports = {
     index: function(req, res){
-        sidebar(model, function(model){
-            res.render('index', model);
+        // get the documents in database
+        imageModel.find({},{},{sort:{timestamp: -1}}, function(err, images) {
+            if(err) throw err;
+            model.images = images;
+            sidebar(model, function(model){
+                res.render('index', model);
+            });
         });
     }
 };
--- controllers/image.js
+++ controllers/image.js
@@ -4,32 +4,8 @@ var sidebar = require('../helpers/sidebar');
 var Model = require('../models/image');
 
 var model = {
-    image: {
-            uniqueId:       1,
-            title:          'Sample Image 1',
-            description:    'Image 1 description',
-            filename:       'image1.jpg',
-            views:          0,
-            likes:          0,
-            timestamp:      Date.now()  
-        }, 
-    comments: [
-        {
-            imageId:        1,
-            email:          'luizleb@gmail.com',
-            name:           'Leb',
-            gravatar:       'http://lorempixel.com/75/75/animals/1',
-            comment:        'This is a test comment.',
-            timestamp:      Date.now()
-        }, {
-            imageId:        1,
-            email:          'deb@gmail.com',
-            name:           'Deb',
-            gravatar:       'http://lorempixel.com/75/75/animals/2',
-            comment:        'This is another test comment.',
-            timestamp:      Date.now()
-        }
-    ]
+    image: {}, 
+    comments: []
 };
 
 module.exports = {
@@ -50,8 +26,7 @@ module.exports = {
                         filename:       shortName + ext
                     });
                     newImage.save(function(err, image) {
-                        res.redirect('/');
-                        //res.redirect('/images/' + image.uniqueId);
+                        res.redirect('/images/' + image.uniqueId);
                     })
                 });
             } else {
@@ -64,8 +39,17 @@ module.exports = {
         saveImage();      
     },
     detail: function(req, res) {
-        sidebar(model, function(model){
-            res.render('image', model);
+        Model.findOne({filename: {$regex: req.params.id}}, function(err, image) {
+            if(err) throw err;
+            if(image) {
+                image.views += 1;
+                model.image = image;
+                image.save();
+                sidebar(model, function(model){
+                    res.render('image', model);
+                });
+            }
+            
         });
     },
     like: function(req, res) {
